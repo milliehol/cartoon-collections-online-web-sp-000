e6e0f48561954f750bd794e0597e69280554418d
Carla Holcomb <carla.programmer@gmail.com>
2021-03-04 19:14:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.any? { |call| call.length > 4 }
 end
