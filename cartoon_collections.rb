4289a4740b1a798c753c23618e4a83645161ab4a
Carla Holcomb <carla.programmer@gmail.com>
2021-03-04 19:18:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,8 @@ end
 
 roll_call_dwarves(dwarves)
 
+planeteer_calls = %w[earth wind fire water heart]
+
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
    planeteer_calls.map { |call| call.capitalize + '!' }
